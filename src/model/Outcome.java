12b4a472b940a440e24a3f6d5f510be98e2e0473
imyanyijie@gmail.com <imyanyijie@gmail.com>
2019-04-01 11:50:45 -0400

    adding DB connection

--- src/model/Outcome.java
+++ src/model/Outcome.java
@@ -4,16 +4,27 @@ public class Outcome {
 	private String outcomeID;
 	private String result;
 	private String receivingService;
+	private RecievingHospital recvHosptial;
 
-	public Outcome(String outcomeID, String result, String receivingService) {
+	public Outcome(String outcomeID, String result, String receivingService,RecievingHospital recvHosptial) {
 		this.outcomeID = outcomeID;
 		this.result = result;
 		this.receivingService = receivingService;
+		this.recvHosptial = recvHosptial;
 	}
 
-	public Outcome(String result, String receivingService) {
+	public RecievingHospital getRecvHosptial() {
+		return recvHosptial;
+	}
+
+	public void setRecvHosptial(RecievingHospital recvHosptial) {
+		this.recvHosptial = recvHosptial;
+	}
+
+	public Outcome(String result, String receivingService,RecievingHospital recvHosptial) {
 		this.result = result;
 		this.receivingService = receivingService;
+		this.recvHosptial = recvHosptial;
 	}
 
 	public String getOutcomeID() {
