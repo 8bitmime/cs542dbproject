36bd17839012bcd55e6516167a7fa1d2b62408e8
8bitmime <bmmattiuzzi@wpi.edu>
2019-04-05 12:19:17 -0400

    put it in git

--- src/cs542dbproject/CSVPaser.java
+++ src/cs542dbproject/CSVPaser.java
@@ -1,46 +1,46 @@
-package cs542dbproject;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-import com.opencsv.CSVReader;
-
-
-public class CSVPaser {
-	private String filePath = null;
-	
-	/**
-	 * ctor
-	 * @param filePath
-	 */
-	public CSVPaser(String filePath) {
-		this.filePath = filePath;
-	}
-	
-	/**
-	 * 
-	 * @return List<String[]> csvRecords
-	 */
-	public List<String[]> readCSV() {
-		List<String[]> csvRecords = null;
-
-		try {
-
-			Reader reader = Files.newBufferedReader(Paths.get(this.filePath));
-			CSVReader csvReader = new CSVReader(reader);
-			csvRecords = csvReader.readAll();
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return csvRecords;
-		
-	}
-	
-	
-}
+//package cs542dbproject;
+//
+//import java.io.IOException;
+//import java.io.Reader;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.List;
+//
+//import com.opencsv.CSVReader;
+//
+//
+//public class CSVPaser {
+//	private String filePath = null;
+//
+//	/**
+//	 * ctor
+//	 * @param filePath
+//	 */
+//	public CSVPaser(String filePath) {
+//		this.filePath = filePath;
+//	}
+//
+//	/**
+//	 *
+//	 * @return List<String[]> csvRecords
+//	 */
+//	public List<String[]> readCSV() {
+//		List<String[]> csvRecords = null;
+//
+//		try {
+//
+//			Reader reader = Files.newBufferedReader(Paths.get(this.filePath));
+//			CSVReader csvReader = new CSVReader(reader);
+//			csvRecords = csvReader.readAll();
+//
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		return csvRecords;
+//
+//	}
+//
+//
+//}
